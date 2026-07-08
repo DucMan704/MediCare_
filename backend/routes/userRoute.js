cbbe1643fd9a62913fabcf33895e2ae4af82552d
Truong Luu <dinhluu1259@gmail.com>
2026-07-09 00:03:38 +0700

    feat: add vnpay endpoint

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,6 +2,9 @@ import express from "express";
 import {
   loginUser,
   registerUser,
+  logoutUser,
+  refreshToken,
+  authMe,
   getProfile,
   updateProfile,
   bookAppointment,
@@ -11,13 +14,18 @@ import {
   verifyRazorpay,
   paymentStripe,
   verifyStripe,
+
 } from "../controllers/userController.js";
+import { paymentVNPay, checkPaymentVNPay } from "../controllers/paymentController.js";
 import upload from "../middleware/multer.js";
 import authUser from "../middleware/authUser.js";
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
+userRouter.post("/logout", logoutUser);
+userRouter.get("/refresh-token", refreshToken);
+userRouter.get("/auth-me", authUser, authMe);
 
 userRouter.get("/get-profile", authUser, getProfile);
 userRouter.post(
@@ -33,5 +41,8 @@ userRouter.post("/payment-razorpay", authUser, paymentRazorpay);
 userRouter.post("/verifyRazorpay", authUser, verifyRazorpay);
 userRouter.post("/payment-stripe", authUser, paymentStripe);
 userRouter.post("/verifyStripe", authUser, verifyStripe);
+
+userRouter.post("/create-vnpay-qr", authUser, paymentVNPay);
+userRouter.post("/check-payment-vnpay", authUser, checkPaymentVNPay);
 
 export default userRouter;
